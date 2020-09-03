e91887f435e8a1155a5c0def8ee06065d50cf255
fevzi ömür tekin <fevziomurtekin@gmail.com>
2020-09-03 11:31:09 +0300

    This error solved. "Cannot get the value of write-only property" fixed #5

--- app/src/test/java/com/fevziomurtekin/hackernewsapp/uitest/MainViewModelTest.kt
+++ app/src/test/java/com/fevziomurtekin/hackernewsapp/uitest/MainViewModelTest.kt
@@ -14,6 +14,7 @@ import org.mockito.MockitoAnnotations
 
 class MainViewModelTest{
 
+    
     lateinit var mainViewModel: MainViewModel
 
     @Mock
@@ -31,7 +32,6 @@ class MainViewModelTest{
         MockitoAnnotations.initMocks(this)
 
         mainViewModel =  MainViewModel(repository,ApplicationSchedulerProvider())
-        mainViewModel.events.observeForever(view)
 
     }
 }
